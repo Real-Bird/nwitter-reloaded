a9136e3f4a014886ff7e9e13f3b8add6949d5ef1
Real-Bird <marbil815@gmail.com>
2023-12-20 17:17:13 +0900

    feat: base setting
    23-12-20

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import GlobalStyles from "./styles/global-styles";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+
 function App() {
-  return <></>;
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
 }
 
 export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4B2Ha3QN9cuO2SEYjGbtUjgOZeKAsX8o",
+  authDomain: "nwitter-reloaded-1f2d4.firebaseapp.com",
+  projectId: "nwitter-reloaded-1f2d4",
+  storageBucket: "nwitter-reloaded-1f2d4.appspot.com",
+  messagingSenderId: "820999891904",
+  appId: "1:820999891904:web:42f140fbebb1d8b92a3625",
+};
+
+const app = initializeApp(firebaseConfig);
