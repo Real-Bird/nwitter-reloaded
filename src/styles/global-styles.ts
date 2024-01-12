fc6305fdc5bf4ed516be7518c37e84fe201b75c6
Real-Bird <marbil815@gmail.com>
2024-01-12 21:34:34 +0900

    feat: real-time tweets
    24-01-12

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -11,6 +11,9 @@ ${reset};
     color:white;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
+  ::-webkit-scrollbar{
+    display:none;
+  }
 `;
 
 export default GlobalStyles;
